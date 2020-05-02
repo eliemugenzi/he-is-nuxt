0a54ae7a7b6d33f4f8b0c0d3e772ecac38baecae
Elie Mugenzi <eliemugenzi@gmail.com>
2020-05-02 19:53:30 +0200

    Display list of jokes

---
+++ components/Joke.vue
@@ -0,0 +1,30 @@
+<template>
+     <a-list-item slot="renderItem" slot-scope="joke">
+      <a-list-item-meta
+      >
+        <a slot="title" href="https://www.antdv.com/">{{ joke.joke }}</a>
+        <a-avatar
+          slot="avatar"
+          src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+        />
+      </a-list-item-meta>
+    </a-list-item>
+</template>
+
+<script>
+export default {
+    name: 'Joke',
+    props: ['joke'],
+    data(){
+        return {
+            item: {
+                title: 'Hello world',
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    
+</style>
--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,15 +1,48 @@
 <template>
   <main>
       <h1>Jokes</h1>
+
+      <a-list item-layout="horizontal" :data-source="jokes" v-if="jokes.length!==0">
+         <div v-for="joke in jokes" :key="joke.id">
+            <joke :joke="joke" />
+         </div>
+      </a-list>
   </main>
 </template>
 
 <script>
+import axios from 'axios';
+import Joke from '../../components/Joke.vue';
 export default {
    head(){
        return {
            title: 'List of Jokes'
        }
+   },
+   data(){
+       return {
+           jokes: [],
+       }
+   },
+   async created(){
+       const config = {
+           headers: {
+               'Accept': 'application/json',
+               'Content-Type': 'application/json'
+           }
+       };
+
+       try{
+          const { data } = await axios.get('https://icanhazdadjoke.com/search', config);
+          console.log('RES', data.results);
+          this.jokes = data.results;
+       } catch(error){
+           console.log(error);
+       }
+
+   },
+   components: {
+       Joke,
    }
 }
 </script>
